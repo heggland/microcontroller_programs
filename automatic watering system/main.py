46cec7121beccea2e152eb8346986aa7bf291e69
heggland <kjetilheggland@gmail.com>
2021-03-24 12:50:57 +0100

    upload
    upload

--- automatic watering system/main.py
+++ automatic watering system/main.py
@@ -4,7 +4,7 @@ import time
 
 moisture_adc = ADC(Pin(26))
 pump_pin = Pin(16, Pin.OUT)
-moisture_lvl = 53000; #set max moisture lvl level here
+moisture_lvl = 53000; 
 
 
 
@@ -22,7 +22,7 @@ def loop():
 
 def water():
 
-    newMoisture_lvl = moisture_lvl - 5000 #set min water moisture lvl here
+    newMoisture_lvl = moisture_lvl - 5000 
     if moisture_adc.read_u16() >= newMoisture_lvl:
         #print(moisture_adc.read_u16(), "water flowing")
         pump_pin.value(0)
